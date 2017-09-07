1858de47635bb937b730c853b8bbb5c854f8dc42
Vincent DO <vincentnhdo@gmail.com>
2017-09-07 14:22:12 -0700

    Homework#1. Done 1-5

--- hw1/hw1-q5-while.sql
+++ hw1/hw1-q5-while.sql
@@ -10,29 +10,16 @@ AS
     SELECT DISTINCT u.src, e.dest, u.length + 1, array_append(u.path, e.dest) AS path
     FROM q5_paths_to_update u, q5_edges e
     WHERE u.dest = e.src AND u.src != e.dest
---    UNION
---    SELECT DISTINCT p.src, e.src, p.length + 1, array_prepend(p.path, e.dest)
---    FROM q5_paths_to_update p, q5_edges e
---    WHERE e.dest = p.dest AND e.src != p.src
---    UNION
---    SELECT DISTINCT p.dest, e.dest, p.length + 1, array_append(p.path, e.src)
---    FROM q5_paths_to_update p, q5_edges e
---    WHERE p.dest = e.src AND p.src != e.dest
 ;
 
 CREATE TABLE q5_new_paths(src, dest, length, path)
 AS
    SELECT e.src, e.dest, e.length, e.path
    FROM q5_extended_paths e
---   WHERE e.src != u.src AND e.dest != u.dest
---   WHERE (SELECT e.src, e.dest) NOT IN (SELECT src, dest FROM q5_paths)
    WHERE NOT EXISTS (SELECT * FROM q5_paths p WHERE p.src = e.src AND p.dest = e.dest)
    UNION
---   SELECT e.src, e.dest, CASE WHEN e.length < u.length THEN e.length ELSE u.length END length, CASE WHEN e.length < u.length THEN e.path ELSE u.path END path
    SELECT e.src, e.dest, e.length, e.path
    FROM q5_extended_paths e, q5_paths p WHERE p.src = e.src AND p.dest = e.dest AND e.length < p.length
---   WHERE e.src = u.src AND e.dest = u.dest
---   WHERE EXISTS (SELECT * FROM q5_paths p WHERE p.src = e.src AND p.dest = e.dest AND e.length < p.length)
 ;
 
 CREATE TABLE q5_better_paths(src, dest, length, path)
@@ -44,6 +31,10 @@ AS
     SELECT p.src, p.dest, p.length, p.path
     FROM q5_paths p
     WHERE NOT EXISTS (SELECT * FROM q5_new_paths n WHERE n.src = p.src AND n.dest = p.dest)
+    UNION
+    SELECT n.src, n.dest, n.length, n.path
+    FROM q5_new_paths n
+    WHERE NOT EXISTS (SELECT * FROM q5_paths p WHERE n.src = p.src AND n.dest = p.dest)
 ;
 
 DROP TABLE q5_paths;
